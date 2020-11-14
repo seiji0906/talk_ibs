12b4b44919ba1ec254b4fe3683063227e8e4ec76
seiji0906 <runrunrun3838@icloud.com>
2020-11-14 16:46:27 +0900

    like

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do
+    resources :likes, only: [:create, :destroy]
+  end
 end
