3e8b329ba81cf980e209d030e8f59be561cb4e8e
seiji0906 <runrunrun3838@icloud.com>
2020-11-16 21:11:56 +0900

    text_field area size

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, length: { in: 1..1000 }
+  validates :title, length: { in: 1..100 }
   validates :body, length: { in: 1..1000 }
   belongs_to :user
   has_many :likes, dependent: :destroy
