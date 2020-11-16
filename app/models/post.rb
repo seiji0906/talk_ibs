e0816662494f03dd5027f8a87d156224d1cbf343
seiji0906 <runrunrun3838@icloud.com>
2020-11-16 18:50:18 +0900

    text size

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, length: { in: 1..1000 }
+  validates :body, length: { in: 1..1000 }
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
