34bf299f3e77d9d56b73381d9b5bb4dd527554ed
seiji0906 <runrunrun3838@icloud.com>
2020-11-16 11:40:47 +0900

    create destroy.js.erb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   def liked_by?(user)
     self.likes.exists?(user_id: user.id)
