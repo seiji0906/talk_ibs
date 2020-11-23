d99ca23342a91cd67975b5aab63c6f4ebc666549
seiji0906 <runrunrun3838@icloud.com>
2020-11-23 19:28:15 +0900

    users/show infinite scroll cannot

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(user_id: @user)
+    @posts = Post.where(user_id: @user).page(params[:page]).per(10)
   end
 end
